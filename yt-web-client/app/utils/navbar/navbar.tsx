519116bc532c4c3848d3a179c8466925898c42bb
nardyy01 <nardyy01@gmail.com>
2023-09-11 17:58:27 -0400

    setup database and login through google

--- yt-web-client/app/utils/navbar/navbar.tsx
+++ yt-web-client/app/utils/navbar/navbar.tsx
@@ -1,9 +1,26 @@
+'use client'
+
 import Image from "next/image";
 import Link from "next/link";
 
 import styles from "./navbar.module.css";
+import SignIn from "../sign-in/sign-in";
+import { onAuthStateChangedHelper } from "../firebase/firebase";
+import { useEffect, useState } from "react";
+import { User } from "firebase/auth";
+import { unsubscribe } from "diagnostics_channel";
 
 export default function Navbar() {
+    const [user, setUser] = useState<User | null>(null);
+    useEffect(() => {
+        const unsubscribe = onAuthStateChangedHelper((user) => {
+            setUser(user);
+        });
+
+        // Cleans up subscription (login) on unmount
+        return () => unsubscribe();
+    });
+
     return (
         <nav className={styles.nav}>
             <Link href="/">
@@ -11,6 +28,10 @@ export default function Navbar() {
                     src="/YouNoob-logo.svg" alt="YouNoob Logo"
                 />
             </Link>
+            {
+                // TODO: Add a upload
+            }
+            <SignIn user={user} />
         </nav>
     )
 }
